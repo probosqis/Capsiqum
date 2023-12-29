f7b3bca0100bf105d8ad60a72185d4c803c127de
wcaokaze <wcaokaze@gmail.com>
2023-12-29 14:31:07 +0900

    moved: com.wcaokaze.probosqis.{page => capsiqum}.*

---
+++ src/androidUnitTest/kotlin/com/wcaokaze/probosqis/capsiqum/transition/TransitionSpecBuilderTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.capsiqum.transition
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import kotlinx.collections.immutable.persistentMapOf
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class TransitionSpecBuilderTest {
+   @get:Rule
+   val rule = createComposeRule()
+
+   @Test
+   fun build() {
+      val enterCurrentId = PageLayoutInfo.LayoutId()
+      val enterCurrent = CurrentPageTransitionElementAnim { Modifier }
+
+      val enterTargetId = PageLayoutInfo.LayoutId()
+      val enterTarget = TargetPageTransitionElementAnim { Modifier }
+
+      val exitCurrentId = PageLayoutInfo.LayoutId()
+      val exitCurrent = CurrentPageTransitionElementAnim { Modifier }
+
+      val exitTargetId = PageLayoutInfo.LayoutId()
+      val exitTarget = TargetPageTransitionElementAnim { Modifier }
+
+      val transitionSpec = pageTransitionSpec(
+         enter = {
+            currentPageElement(enterCurrentId, enterCurrent)
+            targetPageElement (enterTargetId,  enterTarget)
+         },
+         exit = {
+            currentPageElement(exitCurrentId, exitCurrent)
+            targetPageElement (exitTargetId,  exitTarget)
+         }
+      )
+
+      assertEquals(
+         persistentMapOf(enterCurrentId to enterCurrent),
+         transitionSpec.enteringCurrentPageElementAnimations
+      )
+      assertEquals(
+         persistentMapOf(enterTargetId to enterTarget),
+         transitionSpec.enteringTargetPageElementAnimations
+      )
+      assertEquals(
+         persistentMapOf(exitCurrentId to exitCurrent),
+         transitionSpec.exitingCurrentPageElementAnimations
+      )
+      assertEquals(
+         persistentMapOf(exitTargetId to exitTarget),
+         transitionSpec.exitingTargetPageElementAnimations
+      )
+   }
+}
---
+++ src/commonMain/kotlin/com/wcaokaze/probosqis/capsiqum/transition/PageTransitionElementAnim.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.capsiqum.transition
+
+import androidx.compose.animation.core.Transition
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.wcaokaze.probosqis.capsiqum.PageComposable
+import kotlinx.collections.immutable.ImmutableMap
+
+/**
+ * ページ遷移中、[PageComposable.contentComposable]内のComposableに
+ * 適用されるModifier。
+ *
+ * [PageTransitionElementAnimScope.transition]から現在の遷移状況や
+ * 遷移元、遷移先の[PageLayoutInfo]を取得できるため、それを使ってアニメーションを
+ * 生成し、Modifierに適用する。
+ *
+ * @see PageTransitionSpec
+ */
+sealed class PageTransitionElementAnim<S : PageTransitionElementAnimScope> {
+   abstract val createAnimModifier: @Composable S.() -> Modifier
+}
+
+/**
+ * ページ遷移中、遷移元の[PageComposable.contentComposable]内のComposableに
+ * 適用されるModifier。
+ *
+ * @see PageTransitionSpec
+ */
+class CurrentPageTransitionElementAnim(
+   override val createAnimModifier:
+         @Composable CurrentPageTransitionElementAnimScope.() -> Modifier
+) : PageTransitionElementAnim<CurrentPageTransitionElementAnimScope>()
+
+/**
+ * ページ遷移中、遷移先の[PageComposable.contentComposable]内のComposableに
+ * 適用されるModifier。
+ *
+ * @see PageTransitionSpec
+ */
+class TargetPageTransitionElementAnim(
+   override val createAnimModifier:
+         @Composable TargetPageTransitionElementAnimScope.() -> Modifier
+) : PageTransitionElementAnim<TargetPageTransitionElementAnimScope>()
+
+sealed class PageTransitionElementAnimScope {
+   abstract val transition: Transition<PageLayoutInfo>
+
+   val PageLayoutInfo.isCurrentPage: Boolean
+      get() = pageId != transition.targetState.pageId
+   val PageLayoutInfo.isTargetPage: Boolean
+      get() = pageId == transition.targetState.pageId
+}
+
+class CurrentPageTransitionElementAnimScope(
+   override val transition: Transition<PageLayoutInfo>
+) : PageTransitionElementAnimScope()
+
+class TargetPageTransitionElementAnimScope(
+   override val transition: Transition<PageLayoutInfo>
+) : PageTransitionElementAnimScope()
+
+internal typealias PageTransitionElementAnimSet
+      = ImmutableMap<PageLayoutInfo.LayoutId, PageTransitionElementAnim<*>>
+internal typealias CurrentPageTransitionElementAnimSet
+      = ImmutableMap<PageLayoutInfo.LayoutId, CurrentPageTransitionElementAnim>
+internal typealias TargetPageTransitionElementAnimSet
+      = ImmutableMap<PageLayoutInfo.LayoutId, TargetPageTransitionElementAnim>
